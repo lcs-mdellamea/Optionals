b6c6aa96fb2217c0033b35bc4759053d02340e29
Madison Dellamea <mdellamea@lcs.on.ca>
2021-12-05 23:25:43 -0500

    Completed taking notes on pages 1-15.

--- Optionals/Exploring Optionals.playground/Pages/Page 5.xcplaygroundpage/Contents.swift
+++ Optionals/Exploring Optionals.playground/Pages/Page 5.xcplaygroundpage/Contents.swift
@@ -19,8 +19,12 @@
  
  */
 // Type the code below this line...
-
-
+var dayOfWeek: String? = "Monday"
+if dayOfWeek != nil {
+    print("The day is \(dayOfWeek)")
+} else {
+    print("No day was provided")
+}
 /*:
  
  Now change line 22 to look like this:
